52fcf0da9579bbac39935be76e075bd992362e36
chency <ownercloud@sina.com>
2013-12-02 22:55:07 +0800

    仓储信息修改

---
+++ Domain.PurchaseBC/Aggregates/ContractAircraftBFEAgg/IContractAircraftBFERepository.cs
@@ -0,0 +1,27 @@
+﻿#region 版本信息
+
+// =====================================================
+// 版权所有 (C) 2013 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2013/12/02，18:29
+// 方案：FRP
+// 项目：Domain.PurchaseBC
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+namespace UniCloud.Domain.PurchaseBC.Aggregates.ContractAircraftBFEAgg
+{
+    /// <summary>
+    ///     合同飞机BFE仓储接口
+    ///     <see cref="UniCloud.Domain.IRepository{ContractAircraftBFE}" />
+    /// </summary>
+    public interface IContractAircraftBFERepository : IRepository<ContractAircraftBFE>
+    {
+    }
+}
