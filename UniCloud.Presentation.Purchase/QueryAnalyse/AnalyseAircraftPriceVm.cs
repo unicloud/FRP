0cf43dc1f856cc857b092d320f94c392ca561650
linxw <xwlincn@outlook.com>
2013-12-09 18:47:07 +0800

    修改分析页面效果

--- UniCloud.Presentation.Purchase/QueryAnalyse/AnalyseAircraftPriceVm.cs
+++ UniCloud.Presentation.Purchase/QueryAnalyse/AnalyseAircraftPriceVm.cs
@@ -177,7 +177,6 @@ namespace UniCloud.Presentation.Purchase.QueryAnalyse
         public DateTime Date { get; set; }
         public string Name { get; set; }
         public double Close { get; set; }
-
         public long Volume { get; set; }
     }
 
