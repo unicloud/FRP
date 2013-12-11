b90bb92fcea0c3a472961bd6642827ed4c8900e1
dingzi <dingzi@xiamenair.com.cn>
2013-12-11 17:48:23 +0800

    增加币种DTO及相关应用层代码。

---
+++ Application.PurchaseBC.Query/CurrencyQueries/ICurrencyQuery.cs
@@ -0,0 +1,37 @@
+﻿#region 版本信息
+
+// =====================================================
+// 版权所有 (C) 2013 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2013/12/11，17:13
+// 方案：FRP
+// 项目：Application.PurchaseBC.Query
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+#region 命名空间
+
+using System.Linq;
+using UniCloud.Application.PurchaseBC.DTO;
+using UniCloud.Domain.PurchaseBC.Aggregates.CurrencyAgg;
+
+#endregion
+
+namespace UniCloud.Application.PurchaseBC.Query.CurrencyQueries
+{
+    public interface ICurrencyQuery
+    {
+        /// <summary>
+        ///     查询币种
+        /// </summary>
+        /// <param name="query">查询表达式</param>
+        /// <returns>币种DTO集合</returns>
+        IQueryable<CurrencyDTO> CurrenciesQuery(QueryBuilder<Currency> query);
+    }
+}
---
+++ Application.PurchaseBC/CurrencyServices/CurrencyAppService.cs
@@ -0,0 +1,48 @@
+﻿#region 版本信息
+
+// =====================================================
+// 版权所有 (C) 2013 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2013/12/11，17:38
+// 方案：FRP
+// 项目：Application.PurchaseBC
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+#region 命名空间
+
+using System.Linq;
+using UniCloud.Application.PurchaseBC.DTO;
+using UniCloud.Application.PurchaseBC.Query.CurrencyQueries;
+using UniCloud.Domain.PurchaseBC.Aggregates.CurrencyAgg;
+
+#endregion
+
+namespace UniCloud.Application.PurchaseBC.CurrencyServices
+{
+    public class CurrencyAppService : ICurrencyAppService
+    {
+        private readonly ICurrencyQuery _currencyQuery;
+
+        public CurrencyAppService(ICurrencyQuery currencyQuery)
+        {
+            _currencyQuery = currencyQuery;
+        }
+
+        #region ICurrencyAppService 成员
+
+        public IQueryable<CurrencyDTO> GetCurrencies()
+        {
+            var query = new QueryBuilder<Currency>();
+            return _currencyQuery.CurrenciesQuery(query);
+        }
+
+        #endregion
+    }
+}
