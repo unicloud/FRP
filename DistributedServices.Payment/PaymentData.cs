f575bf291f0d970859d1fc1079916f90fa18ff9d
chency <ownercloud@sina.com>
2013-12-16 15:25:24 +0800

    增加币种集合

--- DistributedServices.Payment/PaymentData.cs
+++ DistributedServices.Payment/PaymentData.cs
@@ -1,6 +1,9 @@
 ﻿#region 命名空间
 
 using System.Linq;
+using UniCloud.Application.PaymentBC.ContractAircraftServices;
+using UniCloud.Application.PaymentBC.ContractEngineServices;
+using UniCloud.Application.PaymentBC.CurrencyServices;
 using UniCloud.Application.PaymentBC.DTO;
 using UniCloud.Application.PaymentBC.InvoiceServices;
 using UniCloud.Application.PaymentBC.MaintainInvoiceServices;
@@ -22,7 +25,9 @@ namespace UniCloud.DistributedServices.Payment
         private readonly IMaintainInvoiceAppService _maintainInvoiceAppService;
         private readonly IPrepaymentInvoiceAppService _prepaymentInvoiceAppService;
         private readonly IPurchaseInvoiceAppService _purchaseInvoiceAppService;
-
+        private readonly IContractAircraftAppService _contractAircraftAppService;
+        private readonly IContractEngineAppService _contractEngineAppService;
+        private readonly ICurrencyAppService _currencyAppService;
         public PaymentData()
             : base("UniCloud.Application.PaymentBC.DTO")
         {
@@ -31,6 +36,10 @@ namespace UniCloud.DistributedServices.Payment
             _prepaymentInvoiceAppService = DefaultContainer.Resolve<IPrepaymentInvoiceAppService>();
             _purchaseInvoiceAppService = DefaultContainer.Resolve<IPurchaseInvoiceAppService>();
             _maintainInvoiceAppService = DefaultContainer.Resolve<IMaintainInvoiceAppService>();
+            _contractAircraftAppService = DefaultContainer.Resolve<IContractAircraftAppService>();
+            _contractEngineAppService = DefaultContainer.Resolve<IContractEngineAppService>();
+            _currencyAppService = DefaultContainer.Resolve<ICurrencyAppService>();
+
         }
 
         #region Invoice集合
@@ -103,5 +112,48 @@ namespace UniCloud.DistributedServices.Payment
         }
 
         #endregion
+
+        #region 合同飞机
+
+        /// <summary>
+        ///     合同飞机集合
+        /// </summary>
+        public IQueryable<ContractAircraftDTO> ContractAircrafts
+        {
+            get { return _contractAircraftAppService.GetContractAircrafts(); }
+        }
+
+    
+
+        #endregion
+
+        #region 合同发动机
+
+        /// <summary>
+        ///     合同发动机集合
+        /// </summary>
+        public IQueryable<ContractEngineDTO> ContractEngines
+        {
+            get { return _contractEngineAppService.GetContractEngines(); }
+        }
+
+  
+
+        #endregion
+
+        #region 币种
+
+        /// <summary>
+        ///     币种集合
+        /// </summary>
+        public IQueryable<CurrencyDTO> Currencies
+        {
+            get { return _currencyAppService.GetCurrencies(); }
+        }
+
+        #endregion
+
+
+
     }
 }
