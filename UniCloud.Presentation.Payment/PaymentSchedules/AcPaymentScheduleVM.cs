14079da54483525960235b2d50dafd31dfd8f4a8
chency <ownercloud@sina.com>
2013-12-17 11:06:56 +0800

    修改飞机付款计划

--- UniCloud.Presentation.Payment/PaymentSchedules/AcPaymentScheduleVM.cs
+++ UniCloud.Presentation.Payment/PaymentSchedules/AcPaymentScheduleVM.cs
@@ -77,55 +77,55 @@ namespace UniCloud.Presentation.Payment.PaymentSchedules
 
         #endregion
 
-        #region 加载合同飞机下的付款计划
+        //#region 加载合同飞机下的付款计划
 
-        private AcPaymentScheduleDTO _selectedAcPaymentSchedule;
+        //private AcPaymentScheduleDTO _selectedAcPaymentSchedule;
 
-        /// <summary>
-        ///     选择付款计划
-        /// </summary>
-        public AcPaymentScheduleDTO SelectedAcPaymentSchedule
-        {
-            get { return _selectedAcPaymentSchedule; }
-            set
-            {
-                if (_selectedAcPaymentSchedule != value)
-                {
-                    _selectedAcPaymentSchedule = value;
-                    RaisePropertyChanged(() => SelectedAcPaymentSchedule);
-                }
-            }
-        }
-
-
-        /// <summary>
-        ///     获取所有合同飞机信息。
-        /// </summary>
-        public QueryableDataServiceCollectionView<ContractAircraftDTO> ContractAircraftsView { get; set; }
-
-        /// <summary>
-        ///     初始化合同飞机信息。
-        /// </summary>
-        private void InitialContractAircraft()
-        {
-            ContractAircraftsView = Service.CreateCollection(_context.ContractAircrafts);
-            ContractAircraftsView.PageSize = 20;
-            ContractAircraftsView.LoadedData += (sender, e) =>
-            {
-                if (e.HasError)
-                {
-                    e.MarkErrorAsHandled();
-                    return;
-                }
-                if (SelectedContractAircraft == null)
-                {
-                    SelectedContractAircraft = e.Entities.Cast<ContractAircraftDTO>().FirstOrDefault();
-                }
-            };
-        }
+        ///// <summary>
+        /////     选择付款计划
+        ///// </summary>
+        //public AcPaymentScheduleDTO SelectedAcPaymentSchedule
+        //{
+        //    get { return _selectedAcPaymentSchedule; }
+        //    set
+        //    {
+        //        if (_selectedAcPaymentSchedule != value)
+        //        {
+        //            _selectedAcPaymentSchedule = value;
+        //            RaisePropertyChanged(() => SelectedAcPaymentSchedule);
+        //        }
+        //    }
+        //}
 
 
-        #endregion
+        ///// <summary>
+        /////     获取所有合同飞机信息。
+        ///// </summary>
+        //public QueryableDataServiceCollectionView<ContractAircraftDTO> ContractAircraftsView { get; set; }
+
+        ///// <summary>
+        /////     初始化合同飞机信息。
+        ///// </summary>
+        //private void InitialContractAircraft()
+        //{
+        //    ContractAircraftsView = Service.CreateCollection(_context.ContractAircrafts);
+        //    ContractAircraftsView.PageSize = 20;
+        //    ContractAircraftsView.LoadedData += (sender, e) =>
+        //    {
+        //        if (e.HasError)
+        //        {
+        //            e.MarkErrorAsHandled();
+        //            return;
+        //        }
+        //        if (SelectedContractAircraft == null)
+        //        {
+        //            SelectedContractAircraft = e.Entities.Cast<ContractAircraftDTO>().FirstOrDefault();
+        //        }
+        //    };
+        //}
+
+
+        //#endregion
 
         #region 重载基类服务
 
