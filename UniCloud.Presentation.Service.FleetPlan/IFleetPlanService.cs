0ec5adf6f0d11233a01d358406d05f1ee95d39e0
linxw <xwlincn@outlook.com>
2013-12-29 22:10:11 +0800

    添加文件

---
+++ UniCloud.Presentation.Service.FleetPlan/IFleetPlanService.cs
@@ -0,0 +1,21 @@
+﻿#region Version Info
+/* ========================================================================
+// 版权所有 (C) 2013 UniCloud 
+//【本类功能概述】
+// 
+// 作者：linxw 时间：2013/12/29 14:17:06
+// 文件名：IFleetPlanService
+// 版本：V1.0.0
+//
+// 修改者：linxw 时间：2013/12/29 14:17:06
+// 修改说明：
+// ========================================================================*/
+#endregion
+
+namespace UniCloud.Presentation.Service.FleetPlan
+{
+    public interface IFleetPlanService
+    {
+
+    }
+}
