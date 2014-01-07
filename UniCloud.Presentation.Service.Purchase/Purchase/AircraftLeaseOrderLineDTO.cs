dd291612353602cfb1c52bec85b7c60adcf5b9d6
dingzi <etouch@live.cn>
2014-01-07 22:08:23 +0800

    补充漏提的文件。

---
+++ UniCloud.Presentation.Service.Purchase/Purchase/AircraftLeaseOrderLineDTO.cs
@@ -0,0 +1,40 @@
+﻿#region 版本信息
+
+// =====================================================
+// 版权所有 (C) 2014 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2014/01/07，11:57
+// 方案：FRP
+// 项目：Service.Purchase
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+#region 命名空间
+
+using UniCloud.Presentation.Service.Purchase.Purchase.Enums;
+
+#endregion
+
+namespace UniCloud.Presentation.Service.Purchase.Purchase
+{
+    public partial class AircraftLeaseOrderLineDTO
+    {
+        #region 属性
+
+        /// <summary>
+        ///     合同飞机状态
+        /// </summary>
+        public ContractAircraftStatus ContractAircraftStatus
+        {
+            get { return (ContractAircraftStatus) Status; }
+        }
+
+        #endregion
+    }
+}
