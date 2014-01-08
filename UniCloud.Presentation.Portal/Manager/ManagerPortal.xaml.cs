f2a1b1686d99c8ce72fa05e4b6154de3748a0698
linxw <xwlincn@outlook.com>
2014-01-08 10:44:48 +0800

    修改领导门户

--- UniCloud.Presentation.Portal/Manager/ManagerPortal.xaml.cs
+++ UniCloud.Presentation.Portal/Manager/ManagerPortal.xaml.cs
@@ -6,12 +6,20 @@ using System.ComponentModel.Composition;
 
 namespace UniCloud.Presentation.Portal.Manager
 {
-    [Export]
+    [Export(typeof(ManagerPortal))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
     public partial class ManagerPortal
     {
         public ManagerPortal()
         {
             InitializeComponent();
+        }
+
+        [Import]
+        public ManagerPortalVm ViewModel
+        {
+            get { return DataContext as ManagerPortalVm; }
+            set { DataContext = value; }
         }
     }
 }
