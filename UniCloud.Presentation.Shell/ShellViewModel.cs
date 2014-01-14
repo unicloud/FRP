229be652f67deecc95d04b3038d0401998931a0f
dingzi <etouch@live.cn>
2014-01-14 11:06:31 +0800

    修订项目计划。

--- UniCloud.Presentation.Shell/ShellViewModel.cs
+++ UniCloud.Presentation.Shell/ShellViewModel.cs
@@ -613,7 +613,7 @@ namespace UniCloud.Presentation.Shell
             };
             var menu452 = new MenuItem
             {
-                Text = "维护大修保证金",
+                Text = "维护大修储备金",
                 NavUri = "UniCloud.Presentation.Payment.Guarantees.MaintainGuarantee"
             };
             menu45.Items.Add(menu451);
