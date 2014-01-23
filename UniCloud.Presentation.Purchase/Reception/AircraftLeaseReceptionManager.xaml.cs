cef4d157699b8659b232a56d07f6af92b6d424c1
HuangQiBin <hqbxd1314@gmail.com>
2014-01-23 11:51:31 +0800

    提交接机日程安排控件的编辑窗体修改

--- UniCloud.Presentation.Purchase/Reception/AircraftLeaseReceptionManager.xaml.cs
+++ UniCloud.Presentation.Purchase/Reception/AircraftLeaseReceptionManager.xaml.cs
@@ -1,18 +1,6 @@
 ﻿
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media.Animation;
-using Telerik.Windows.Controls;
-using Telerik.Windows.Controls.ScheduleView;
-using Telerik.Windows.Documents.Fixed.Model;
 
 namespace UniCloud.Presentation.Purchase.Reception
 {
