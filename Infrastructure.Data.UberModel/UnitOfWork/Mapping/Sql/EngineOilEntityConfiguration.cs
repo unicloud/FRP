e1a05438bb1533f015d845616fbff1baef76441b
dingzi <etouch@live.cn>
2014-02-22 23:28:11 +0800

    补提漏提的文件。

---
+++ Infrastructure.Data.UberModel/UnitOfWork/Mapping/Sql/EngineOilEntityConfiguration.cs
@@ -0,0 +1,41 @@
+﻿#region 版本信息
+
+// =====================================================
+// 版权所有 (C) 2014 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2014/02/22，21:12
+// 方案：FRP
+// 项目：Infrastructure.Data.PartBC
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+#region 命名空间
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using UniCloud.Domain.UberModel.Aggregates.OilMonitorAgg;
+
+#endregion
+
+namespace UniCloud.Infrastructure.Data.UberModel.UnitOfWork.Mapping.Sql
+{
+    /// <summary>
+    ///     EngineOil实体相关配置
+    /// </summary>
+    internal class EngineOilEntityConfiguration : EntityTypeConfiguration<EngineOil>
+    {
+        public EngineOilEntityConfiguration()
+        {
+            ToTable("EngineOil", DbConfig.Schema);
+
+            HasKey(p => p.Id);
+            Property(p => p.Id).HasColumnName("ID").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+}
