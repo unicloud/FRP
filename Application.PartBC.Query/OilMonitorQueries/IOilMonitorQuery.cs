b2b874ce55d30291958415bfa1860c751d3b67ec
dingzi <etouch@live.cn>
2014-02-23 13:37:30 +0800

    滑油监控应用层代码。

---
+++ Application.PartBC.Query/OilMonitorQueries/IOilMonitorQuery.cs
@@ -0,0 +1,29 @@
+﻿#region 版本信息
+// =====================================================
+// 版权所有 (C) 2014 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2014/02/23，11:39
+// 方案：FRP
+// 项目：Application.PartBC.Query
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+#endregion
+
+using System.Linq;
+using UniCloud.Domain.PartBC.Aggregates.OilMonitorAgg;
+
+namespace UniCloud.Application.PartBC.Query.OilMonitorQueries
+{
+    /// <summary>
+    /// 滑油监控查询
+    /// </summary>
+    public interface IOilMonitorQuery
+    {
+        //IQueryable<EngineOil> EngineOilDTOQuery(QueryBuilder<EngineOil> query);
+
+    }
+}
---
+++ Application.PartBC.Query/OilMonitorQueries/OilMonitorQuery.cs
@@ -0,0 +1,31 @@
+﻿#region 版本信息
+// =====================================================
+// 版权所有 (C) 2014 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：2014/02/23，11:39
+// 方案：FRP
+// 项目：Application.PartBC.Query
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+#endregion
+
+using UniCloud.Infrastructure.Data;
+
+namespace UniCloud.Application.PartBC.Query.OilMonitorQueries
+{
+    public class OilMonitorQuery : IOilMonitorQuery
+    {
+        private readonly IQueryableUnitOfWork _unitOfWork;
+
+        public OilMonitorQuery(IQueryableUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+         
+    }
+}
