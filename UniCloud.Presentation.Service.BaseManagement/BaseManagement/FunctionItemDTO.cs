aa2c58ba2c2cdff478d647734d9c3d5d3608839c
linxw <xwlincn@outlook.com>
2014-03-17 18:11:02 +0800

    修改用户界面

--- UniCloud.Presentation.Service.BaseManagement/BaseManagement/FunctionItemDTO.cs
+++ UniCloud.Presentation.Service.BaseManagement/BaseManagement/FunctionItemDTO.cs
@@ -11,17 +11,6 @@
 // 修改说明：
 // ========================================================================*/
 #endregion
-using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Automation;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace UniCloud.Presentation.Service.BaseManagement.BaseManagement
 {
---
+++ UniCloud.Presentation.Service.BaseManagement/BaseManagement/UserDTO.cs
@@ -0,0 +1,36 @@
+﻿#region Version Info
+/* ========================================================================
+// 版权所有 (C) 2014 UniCloud 
+//【本类功能概述】
+// 
+// 作者：linxw 时间：2014/3/17 18:01:33
+// 文件名：UserDTO
+// 版本：V1.0.0
+//
+// 修改者：linxw 时间：2014/3/17 18:01:33
+// 修改说明：
+// ========================================================================*/
+#endregion
+
+#region 命名空间
+
+using System;
+using System.Text.RegularExpressions;
+
+#endregion
+
+namespace UniCloud.Presentation.Service.BaseManagement.BaseManagement
+{
+    public partial class UserDTO
+    {
+        partial void OnEmailChanging(string value)
+        {
+            //RFC2822 http://en.wikipedia.org/wiki/Email_address 
+            if (!string.IsNullOrEmpty(value) &&
+                !Regex.IsMatch(value, @"^((?'name'.+?)\s*<)?(?'email'(?>[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+\x20*|""(?'user'(?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*""\x20*)*(?'angle'<))?(?'user'(?!\.)(?>\.?[a-zA-Z\d!#$%&'*+\-/=?^_`{|}~]+)+|""((?=[\x01-\x7f])[^""\\]|\\[\x01-\x7f])*"")@(?'domain'((?!-)[a-zA-Z\d\-]+(?<!-)\.)+[a-zA-Z]{2,}|\[(((?(?<!\[)\.)(25[0-5]|2[0-4]\d|[01]?\d?\d)){4}|[a-zA-Z\d\-]*[a-zA-Z\d]:((?=[\x01-\x7f])[^\\\[\]]|\\[\x01-\x7f])+)\])(?'angle')(?(name)>)$"))
+            {
+                throw new Exception("邮箱格式有误");
+            }
+        }
+    }
+}
