eebb0cb600467ff92d75450add45d44c0c95cd9e
HuangQiBin <hqbxd1314@gmail.com>
2014-04-09 17:49:51 +0800

    提交管理附件项、管理附件的三个界面

---
+++ UniCloud.Presentation.Part/ManageItem/ItemControlVm.cs
@@ -0,0 +1,492 @@
+﻿#region 版本信息
+/* ========================================================================
+// 版权所有 (C) 2014 UniCloud 
+//【本类功能概述】
+// 
+// 作者：HuangQiBin 时间：2014/4/8 16:54:22
+// 文件名：ItemControlVm
+// 版本：V1.0.0
+//
+// 修改者： 时间： 
+// 修改说明：
+// ========================================================================*/
+#endregion
+
+#region 命名空间
+
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.Practices.Prism;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Regions;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Data;
+using UniCloud.Presentation.CommonExtension;
+using UniCloud.Presentation.MVVM;
+using UniCloud.Presentation.Service.Part;
+using UniCloud.Presentation.Service.Part.Part;
+
+#endregion
+
+namespace UniCloud.Presentation.Part.ManageItem
+{
+    [Export(typeof(ItemControlVm))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class ItemControlVm : EditViewModelBase
+    {
+        #region 声明、初始化
+
+        private readonly PartData _context;
+        private readonly IRegionManager _regionManager;
+        private readonly IPartService _service;
+        private bool _addToInstallController = false;
+        private bool _addToDependency = false;
+
+
+        [ImportingConstructor]
+        public ItemControlVm(IRegionManager regionManager, IPartService service)
+            : base(service)
+        {
+            _regionManager = regionManager;
+            _service = service;
+            _context = _service.Context;
+            InitializeVM();
+            InitializerCommand();
+        }
+
+        /// <summary>
+        ///     初始化ViewModel
+        ///     <remarks>
+        ///         统一在此处创建并注册CollectionView集合。
+        ///     </remarks>
+        /// </summary>
+        private void InitializeVM()
+        {
+            InstallControllers = _service.CreateCollection(_context.InstallControllers, o => o.Dependencies);
+            InstallControllers.GroupDescriptors.Add(new GroupDescriptor { Member = "AircraftTypeName", SortDirection = ListSortDirection.Ascending });
+            InstallControllers.GroupDescriptors.Add(new GroupDescriptor { Member = "ItemNo", SortDirection = ListSortDirection.Ascending });
+            _service.RegisterCollectionView(InstallControllers);
+
+            Items = new QueryableDataServiceCollectionView<ItemDTO>(_context, _context.Items);
+            PnRegs = new QueryableDataServiceCollectionView<PnRegDTO>(_context, _context.PnRegs);
+            AircraftTypes = new QueryableDataServiceCollectionView<AircraftTypeDTO>(_context, _context.AircraftTypes);
+        }
+
+        /// <summary>
+        ///     初始化命令。
+        /// </summary>
+        private void InitializerCommand()
+        {
+            NewCommand = new DelegateCommand<object>(OnNew, CanNew);
+            RemoveCommand = new DelegateCommand<object>(OnRemove, CanRemove);
+            AddDependencyCommand = new DelegateCommand<object>(OnAddDependency, CanAddDependency);
+            RemoveDependencyCommand = new DelegateCommand<object>(OnRemoveDependency, CanRemoveDependency);
+            CommitCommand = new DelegateCommand<object>(OnCommitExecute, CanCommitExecute);
+            CancelCommand = new DelegateCommand<object>(OnCancelExecute, CanCancelExecute);
+        }
+
+        #endregion
+
+        #region 数据
+
+        #region 公共属性
+
+        #region 附件集合
+        private ObservableCollection<PnRegDTO> _viewPnRegs = new ObservableCollection<PnRegDTO>();
+
+        /// <summary>
+        ///     附件集合
+        /// </summary>
+        public QueryableDataServiceCollectionView<PnRegDTO> PnRegs { get; set; }
+
+        /// <summary>
+        ///     选择的机型
+        /// </summary>
+        public ObservableCollection<PnRegDTO> ViewPnRegs
+        {
+            get { return this._viewPnRegs; }
+            private set
+            {
+                if (this._viewPnRegs != value)
+                {
+                    this._viewPnRegs = value;
+                    RaisePropertyChanged(() => this.ViewPnRegs);
+                }
+            }
+        }
+        #endregion
+
+        #region 机型集合
+
+        private AircraftTypeDTO _selAircraftType;
+
+        /// <summary>
+        ///     机型集合
+        /// </summary>
+        public QueryableDataServiceCollectionView<AircraftTypeDTO> AircraftTypes { get; set; }
+
+        /// <summary>
+        ///     选择的机型
+        /// </summary>
+        public AircraftTypeDTO SelAircraftType
+        {
+            get { return this._selAircraftType; }
+            private set
+            {
+                if (this._selAircraftType != value)
+                {
+                    this._selAircraftType = value;
+                    RaisePropertyChanged(() => this.SelAircraftType);
+                    RefreshCommandState();
+                }
+            }
+        }
+
+        #endregion
+
+        #region 附件项
+
+        private ItemDTO _selItem;
+
+        /// <summary>
+        ///     附件项集合
+        /// </summary>
+        public QueryableDataServiceCollectionView<ItemDTO> Items { get; set; }
+
+        /// <summary>
+        ///     选择的附件项
+        /// </summary>
+        public ItemDTO SelItem
+        {
+            get { return this._selItem; }
+            private set
+            {
+                if (this._selItem != value)
+                {
+                    this._selItem = value;
+                    RaisePropertyChanged(() => this.SelItem);
+                    RefreshCommandState();
+                }
+            }
+        }
+
+        #endregion
+        #endregion
+
+        #region 加载数据
+
+        /// <summary>
+        ///     加载数据方法
+        ///     <remarks>
+        ///         导航到此页面时调用。
+        ///         可在此处将CollectionView的AutoLoad属性设为True，以实现数据的自动加载。
+        ///     </remarks>
+        /// </summary>
+        public override void LoadData()
+        {
+            PnRegs.Load(true);
+            AircraftTypes.Load(true);
+            Items.Load(true);
+
+            if (!InstallControllers.AutoLoad)
+                InstallControllers.AutoLoad = true;
+            InstallControllers.Load(true);
+        }
+
+        #region 业务
+
+        #region 装机控制集合
+
+        private InstallControllerDTO _selInstallController;
+
+        /// <summary>
+        ///     装机控制集合
+        /// </summary>
+        public QueryableDataServiceCollectionView<InstallControllerDTO> InstallControllers { get; set; }
+
+        /// <summary>
+        /// 选择的装机控制
+        /// </summary>
+        public InstallControllerDTO SelInstallController
+        {
+            get { return this._selInstallController; }
+            private set
+            {
+                if (this._selInstallController != value)
+                {
+                    _selInstallController = value;
+                    this.RaisePropertyChanged(() => this.SelInstallController);
+                }
+                RefreshCommandState();
+            }
+        }
+
+        #endregion
+
+        #region 选择的依赖项
+
+        private DependencyDTO _selDependency;
+
+        /// <summary>
+        /// 选择的依赖项
+        /// </summary>
+        public DependencyDTO SelDependency
+        {
+            get { return this._selDependency; }
+            private set
+            {
+                if (this._selDependency != value)
+                {
+                    _selDependency = value;
+                    this.RaisePropertyChanged(() => this.SelDependency);
+                }
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        #region 操作
+
+        #region 刷新按钮状态
+
+        protected override void RefreshCommandState()
+        {
+            NewCommand.RaiseCanExecuteChanged();
+            RemoveCommand.RaiseCanExecuteChanged();
+            AddDependencyCommand.RaiseCanExecuteChanged();
+            RemoveDependencyCommand.RaiseCanExecuteChanged();
+        }
+
+        #endregion
+
+        #region 增加互换件
+
+        /// <summary>
+        ///     增加互换件
+        /// </summary>
+        public DelegateCommand<object> NewCommand { get; private set; }
+
+        private void OnNew(object obj)
+        {
+            if (SelItem == null)
+                MessageAlert("提示", "请先选中附件项！");
+            else if (SelAircraftType == null)
+                MessageAlert("提示", "请先选中机型！");
+            else
+            {
+                ViewPnRegs = new ObservableCollection<PnRegDTO>();
+                if (SelItem != null)
+                {
+                    ViewPnRegs.AddRange(
+                        PnRegs.Where(p => p.IsLife == SelItem.IsLife && (p.ItemId == SelItem.Id || p.ItemId == null)));
+                }
+                _addToInstallController = true;
+                _addToDependency = false;
+                PnRegsChildView.ShowDialog();
+            }
+            RefreshCommandState();
+        }
+
+        private bool CanNew(object obj)
+        {
+            if (SelItem != null && SelAircraftType != null)
+                return true;
+            return false;
+        }
+
+        #endregion
+
+        #region 移除互换件
+
+        /// <summary>
+        ///     移除互换件
+        /// </summary>
+        public DelegateCommand<object> RemoveCommand { get; private set; }
+
+        private void OnRemove(object obj)
+        {
+            InstallControllers.Remove(SelInstallController);
+            RefreshCommandState();
+        }
+
+        private bool CanRemove(object obj)
+        {
+            if (SelInstallController != null)
+                return true;
+            return false;
+        }
+
+        #endregion
+
+        #region 增加依赖项
+
+        /// <summary>
+        ///     增加依赖项
+        /// </summary>
+        public DelegateCommand<object> AddDependencyCommand { get; private set; }
+
+        private void OnAddDependency(object obj)
+        {
+            if (SelInstallController == null)
+                MessageAlert("提示", "请先选中装机控制项！");
+            else
+            {
+                ViewPnRegs = new ObservableCollection<PnRegDTO>();
+                ViewPnRegs.AddRange(PnRegs);
+                _addToDependency = true;
+                _addToInstallController = false;
+                PnRegsChildView.ShowDialog();
+            }
+            RefreshCommandState();
+        }
+
+        private bool CanAddDependency(object obj)
+        {
+            if (SelInstallController == null)
+                return false;
+            return true;
+        }
+
+        #endregion
+
+        #region 移除依赖项
+
+        /// <summary>
+        ///     移除依赖项
+        /// </summary>
+        public DelegateCommand<object> RemoveDependencyCommand { get; private set; }
+
+        private void OnRemoveDependency(object obj)
+        {
+            SelInstallController.Dependencies.Remove(SelDependency);
+            RefreshCommandState();
+        }
+
+        private bool CanRemoveDependency(object obj)
+        {
+            if (SelInstallController != null && SelDependency != null)
+                return true;
+            return false;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region 子窗体相关
+
+        [Import]
+        public PnRegsChildView PnRegsChildView; //初始化子窗体
+
+
+        #region 命令
+
+        #region 取消命令
+
+        public DelegateCommand<object> CancelCommand { get; private set; }
+
+        /// <summary>
+        ///     执行取消命令。
+        /// </summary>
+        /// <param name="sender"></param>
+        public void OnCancelExecute(object sender)
+        {
+            _addToInstallController = false;
+            _addToDependency = false;
+            PnRegsChildView.Close();
+        }
+
+        /// <summary>
+        ///     判断取消命令是否可用。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <returns>取消命令是否可用。</returns>
+        public bool CanCancelExecute(object sender)
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region 确定命令
+
+        public DelegateCommand<object> CommitCommand { get; private set; }
+
+        /// <summary>
+        ///     执行确定命令。
+        /// </summary>
+        /// <param name="sender"></param>
+        public void OnCommitExecute(object sender)
+        {
+            var radGridView = sender as RadGridView;
+            if (radGridView == null) return;
+            if (SelItem != null && SelAircraftType != null && _addToInstallController)
+            {
+                radGridView.SelectedItems.ToList().ForEach(p =>
+                {
+                    var pnRegDTO = p as PnRegDTO;
+                    if (pnRegDTO != null)
+                    {
+                        var installController = new InstallControllerDTO
+                        {
+                            Id = RandomHelper.Next(),
+                            Pn = pnRegDTO.Pn,
+                            PnRegId = pnRegDTO.Id,
+                            ItemId = SelItem.Id,
+                            ItemNo = SelItem.ItemNo,
+                            Description = pnRegDTO.Description,
+                            AircraftTypeId = SelAircraftType.Id,
+                            AircraftTypeName = SelAircraftType.Name,
+                            StartDate = DateTime.Now,
+                        };
+                        InstallControllers.AddNew(installController);
+                    }
+                });
+                _addToInstallController = false;
+            }
+            else if (SelInstallController != null && _addToDependency)
+            {
+                radGridView.SelectedItems.ToList().ForEach(p =>
+                {
+                    var pnRegDTO = p as PnRegDTO;
+                    if (pnRegDTO != null)
+                    {
+                        var dependency = new DependencyDTO()
+                        {
+                            Id = RandomHelper.Next(),
+                            Pn = pnRegDTO.Pn,
+                            DependencyPnId = pnRegDTO.Id,
+                            InstallControllerId = SelInstallController.Id,
+                        };
+                        SelInstallController.Dependencies.Add(dependency);
+                    }
+                });
+                _addToDependency = false;
+            }
+            PnRegsChildView.Close();
+        }
+
+        /// <summary>
+        ///     判断确定命令是否可用。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <returns>确定命令是否可用。</returns>
+        public bool CanCommitExecute(object sender)
+        {
+            return true;
+        }
+
+        #endregion
+        #endregion
+        #endregion
+    }
+}
