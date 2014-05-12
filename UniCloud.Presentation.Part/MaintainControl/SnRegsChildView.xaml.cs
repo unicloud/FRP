0e55174a08a2206b3f5faf5c32252795b918a257
HuangQiBin <hqbxd1314@gmail.com>
2014-05-12 17:25:40 +0800

    维护拆装记录界面修改

---
+++ UniCloud.Presentation.Part/MaintainControl/SnRegsChildView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace UniCloud.Presentation.Part.MaintainControl
+{
+    [Export(typeof (SnRegsChildView))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public partial class SnRegsChildView
+    {
+        public SnRegsChildView()
+        {
+            InitializeComponent();
+        }
+
+        [Import]
+        public ManageRemovalAndInstallationVm ViewModel
+        {
+            get { return DataContext as ManageRemovalAndInstallationVm; }
+            set { DataContext = value; }
+        }
+    }
+}
