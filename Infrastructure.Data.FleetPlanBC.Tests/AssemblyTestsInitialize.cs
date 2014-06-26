f07bd9d473028881c1847b2fb483a8b1c9d21c6b
dingzi <etouch@live.cn>
2014-06-26 17:21:13 +0800

    运力规划仓储实现测试方法

---
+++ Infrastructure.Data.FleetPlanBC.Tests/AssemblyTestsInitialize.cs
@@ -0,0 +1,59 @@
+﻿#region 版本控制
+
+// =====================================================
+// 版权所有 (C) 2014 UniCloud 
+// 【本类功能概述】
+// 
+// 作者：丁志浩 时间：16:55
+// 方案：FRP
+// 项目：Infrastructure.Data.FleetPlanBC.Tests
+// 版本：V1.0.0
+// 
+// 修改者： 时间： 
+// 修改说明：
+// =====================================================
+
+#endregion
+
+#region 命名空间
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniCloud.Domain.FleetPlanBC.Aggregates.AircraftTypeAgg;
+using UniCloud.Domain.FleetPlanBC.Aggregates.AirlinesAgg;
+using UniCloud.Domain.FleetPlanBC.Aggregates.PlanAircraftAgg;
+using UniCloud.Infrastructure.Data.FleetPlanBC.Repositories;
+using UniCloud.Infrastructure.Data.FleetPlanBC.UnitOfWork;
+using UniCloud.Infrastructure.Unity;
+
+#endregion
+
+namespace UniCloud.Infrastructure.Data.FleetPlanBC.Tests
+{
+    [TestClass]
+    public class AssemblyTestsInitialize
+    {
+        /// <summary>
+        ///     初始化测试用的Unity容器
+        /// </summary>
+        /// <param name="context">MS TEST 上下文</param>
+        [AssemblyInitialize]
+        public static void InitializeContainer(TestContext context)
+        {
+            UniContainer.Create()
+                .Register<IQueryableUnitOfWork, FleetPlanBCUnitOfWork>(new WcfPerRequestLifetimeManager())
+                .Register<IModelConfiguration, SqlConfigurations>("Sql")
+                .Register<IAirlinesRepository, AirlinesRepository>()
+                .Register<IAircraftTypeRepository, AircraftTypeRepository>()
+                .Register<IPlanAircraftRepository, PlanAircraftRepository>();
+        }
+
+        /// <summary>
+        ///     清理测试用的Unity容器
+        /// </summary>
+        [AssemblyCleanup]
+        public static void CleanupContainer()
+        {
+            UniContainer.Cleanup();
+        }
+    }
+}
